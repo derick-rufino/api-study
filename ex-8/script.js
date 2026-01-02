0b00661287dd786392234cef48950af4d1011bb9
derick-rufino <derick10011@gmail.com>
2026-01-02 17:51:46 -0300

    feat: adiciona funcionalidade de buscador de piadas com exibição de setup e punchline

--- ex-8/script.js
+++ ex-8/script.js
@@ -3,3 +3,30 @@
 // ? https://official-joke-api.appspot.com/random_joke
 // ? Exiba o setup e o punchline
 // ? Use async/await
+
+const url = "https://official-joke-api.appspot.com/random_joke";
+document.getElementById("getJoke").addEventListener("click", renderJoke);
+
+async function renderJoke() {
+  const joke = await getJoke();
+
+  document.getElementById("joke-setup").innerText = joke.setup;
+
+  setTimeout(() => {
+    document.getElementById("joke-punchline").innerText = joke.punchline;
+  }, 2000);
+}
+
+async function getJoke() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
